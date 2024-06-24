0835e382a93b6b30485e083a68ac9dcafeaa0737
Gunjan <vidyasagargunjan861@gmail.com>
2024-06-24 14:25:05 +0530

    Initial Commit

---
+++ book_store/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  book_store
+//
+//  Created by Gunjan Vidya Sagar on 24/06/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
